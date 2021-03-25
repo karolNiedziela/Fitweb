bb8eb56fa2cf86dec434a30310e9a192cc3ca504
karolNiedziela <imniedziel@gmail.com>
2021-03-25 21:41:44 +0100

    Add UserWithEmailNotFoundException

---
+++ Backend/src/Backend.Infrastructure/Exceptions/UserWithEmailNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Infrastructure.Exceptions
+{
+    public class UserWithEmailNotFoundException : InfrastructureException
+    {
+        public UserWithEmailNotFoundException(string email) : base($"User with '{email}' was not found.")
+        {
+        }
+    }
+}

ad7bbc3649a23920e2d58cbf657c864b7c6fb016
karolNiedziela <imniedziel@gmail.com>
2021-04-05 16:10:23 +0200

    Add proper position for alerts

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Fitweb';
+
+  ngOnInit(): void {
+    this.positionAlert();
+  }
+
+  positionAlert(): void {
+    let nav = document.querySelector('nav');
+
+    let navCoords = nav.getBoundingClientRect();
+
+    let alert = document.querySelector<HTMLElement>('app-alert');
+    alert.style.top = navCoords.top + nav.offsetHeight + 'px';
+    alert.style.right = navCoords.left + 'px';
+  }
 }

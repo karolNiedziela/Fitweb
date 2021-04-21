f6792101ba68547b631061948df58e7e5b7d607c
karolNiedziela <imniedziel@gmail.com>
2021-04-21 19:41:31 +0200

    Add athletes diet controller

---
+++ Backend/src/Backend.Api/Controllers/AthletesDietStatsController.cs
@@ -0,0 +1,31 @@
+﻿using Backend.Infrastructure.CommandQueryHandler;
+using Backend.Infrastructure.CommandQueryHandler.Queries.Athletes;
+using Backend.Infrastructure.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Api.Controllers
+{
+    [ApiController]
+    [Route("api/athletes/dietstats")]
+    [Authorize]
+    public class AthletesDietStatsController : ApiControllerBase
+    {
+        public AthletesDietStatsController(IDispatcher dispatcher) : base(dispatcher)
+        {
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<AthleteDto>> GetAsync(DateTime? date)
+        {
+            var athlete = await QueryAsync(new GetDietStats(UserId, date));
+
+
+            return Ok(athlete);
+        }
+    }
+}

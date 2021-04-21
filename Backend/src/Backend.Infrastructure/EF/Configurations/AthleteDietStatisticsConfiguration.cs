0a8ae877a916b0c567fa882961c69cc8edf4e1a9
karolNiedziela <imniedziel@gmail.com>
2021-04-21 19:49:18 +0200

    Add new configurations

---
+++ Backend/src/Backend.Infrastructure/EF/Configurations/AthleteDietStatisticsConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Backend.Core.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Infrastructure.EF.Configurations
+{
+    public class AthleteDietStatisticsConfiguration : BaseEntityConfiguration<AthleteDietStats>
+    {
+        public override void Configure(EntityTypeBuilder<AthleteDietStats> builder)
+        {
+            builder.HasIndex(ads => new {ads.UserId , ads.DietStatId })
+                .IsUnique(false);
+
+            builder.HasOne(ads => ads.Athlete)
+                   .WithMany(a => a.AthleteDietStats)
+                   .HasForeignKey(ads => ads.UserId);
+        }
+    }
+}
---
+++ Backend/src/Backend.Infrastructure/EF/Configurations/DietStatisticsConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Backend.Core.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Infrastructure.EF.Configurations
+{
+    public class DietStatisticsConfiguration : BaseEntityConfiguration<DietStat>
+    {
+        public override void Configure(EntityTypeBuilder<DietStat> builder)
+        {
+            builder.Property(cd => cd.TotalCalories).IsRequired();
+            builder.Property(cd => cd.TotalProteins).IsRequired();
+            builder.Property(cd => cd.TotalCarbohydrates).IsRequired();
+            builder.Property(cd => cd.TotalFats).IsRequired();
+        }
+    }
+}

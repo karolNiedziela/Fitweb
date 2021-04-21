5d5d12137a451f9af312bfbcc08a82ce9aaa4b21
karolNiedziela <imniedziel@gmail.com>
2021-04-21 19:48:18 +0200

    Add dtos

---
+++ Backend/src/Backend.Infrastructure/DTO/DietStatDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Infrastructure.DTO
+{
+    public class DietStatDto
+    {
+        public double TotalCalories { get; set; }
+
+        public double TotalProteins { get; set; }
+
+        public double TotalCarbohydrates { get; set; }
+
+        public double TotalFats { get; set; }
+
+        public string DateCreated { get; set; }
+
+    }
+}

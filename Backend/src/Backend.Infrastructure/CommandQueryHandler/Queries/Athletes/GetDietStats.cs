c36c5c52ca950c799e632f49e5902200b6e7f438
karolNiedziela <imniedziel@gmail.com>
2021-04-21 19:47:05 +0200

    Add query for diet stats

---
+++ Backend/src/Backend.Infrastructure/CommandQueryHandler/Queries/Athletes/GetDietStats.cs
@@ -0,0 +1,22 @@
+﻿using Backend.Infrastructure.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Infrastructure.CommandQueryHandler.Queries.Athletes
+{
+    public class GetDietStats : IQuery<AthleteDto>
+    {
+        public int UserId { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public GetDietStats(int userId, DateTime? date)
+        {
+            UserId = userId;
+            Date = date;
+        }
+    }
+}

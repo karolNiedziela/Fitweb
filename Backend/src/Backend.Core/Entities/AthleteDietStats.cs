dcc942c56d2dce5915e224218da81be07409ac9e
karolNiedziela <imniedziel@gmail.com>
2021-04-21 19:46:30 +0200

    Add new entities

---
+++ Backend/src/Backend.Core/Entities/AthleteDietStats.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Core.Entities
+{
+    public class AthleteDietStats : BaseEntity
+    {
+        public Athlete Athlete { get; set; }
+
+        public int UserId { get; set; }
+
+        public DietStat DietStat { get; set; }
+
+        public int DietStatId { get; set; }
+
+        public AthleteDietStats()
+        {
+
+        }
+
+        public AthleteDietStats(Athlete athlete, DietStat dietStat)
+        {
+            Athlete = athlete;
+            DietStat = dietStat;
+        }
+
+
+        public static AthleteDietStats Create(Athlete athlete, DietStat dietStat) =>
+            new AthleteDietStats(athlete, dietStat);
+    }
+}
---
+++ Backend/src/Backend.Core/Entities/DietStat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Core.Entities
+{
+    public class DietStat : BaseEntity
+    {
+        public double TotalCalories { get; set; } = 0;
+
+        public double TotalProteins { get; set; } = 0;
+
+        public double TotalCarbohydrates { get; set; } = 0;
+
+        public double TotalFats { get; set; } = 0;
+
+
+        public DietStat()
+        {
+
+        }
+
+        public DietStat(double totalCalories, double totalProteins, double totalCarbohydrates, double totalFats)
+        {
+            TotalCalories = totalCalories;
+            TotalProteins = totalProteins;
+            TotalCarbohydrates = totalCarbohydrates;
+            TotalFats = totalFats;
+        }
+
+
+
+    }
+}
